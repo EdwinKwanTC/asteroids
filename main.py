3130ea4df52599c4ef68958c615ce6076cc313c7
Edwin Kwan <edwinkwantc@gmail.com>
2025-01-29 22:55:31 +0800

    feat: updating the clock frame

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 
 def main():
-    print("Starting asteroids!")
-    print("Screen width: " + str(SCREEN_WIDTH))
-    print("Screen height: " + str(SCREEN_HEIGHT))
     pygame.init()
-
-if __name__ == "__main__":
+    clock = pygame.time.Clock()
+    tick = clock.tick(60)
+    dt = tick / 1000.0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    screen.fill((0, 0, 0))
-    pygame.display.flip()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                break
-        main()
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
