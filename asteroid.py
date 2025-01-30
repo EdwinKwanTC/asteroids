4c630fc7d4e3c74165c51a1f9687d2e0f726b2a0
Edwin Kwan <edwinkwantc@gmail.com>
2025-01-30 17:46:47 +0800

    feat: adding shot class

--- asteroid.py
+++ asteroid.py
@@ -4,8 +4,6 @@ from circleshape import CircleShape
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
-        self.x = x
-        self.y = y
 
     def draw(self, screen):
         return pygame.draw.circle(screen, 'white', self.position, self.radius, 2)
